042e7e3fe4e7684c311118d68c21ba58beaaa412
Hannes Kirsman <hannes.kirsman@wunderkraut.com>
2016-08-23 23:02:37 +0300

    Small textual fix for the popup.

--- popup.js
+++ popup.js
@@ -10,12 +10,12 @@ var msg = '' +
     '</div>' +
     '<div style="color:' + (bg.getBillablePercentWeek() > bg.config.minBillablePercent ? 'green': 'red') + '">' +
       '<h2>Week</h2>' +
-      'Total: ' + round(bg.times.week.billable + bg.times.week.nonBillable) + 'h /' + (bg.config.minDayHours * 5) + 'h<br>' +
+      'Total: ' + round(bg.times.week.billable + bg.times.week.nonBillable) + 'h / ' + (bg.config.minDayHours * 5) + 'h<br>' +
       'Billable: ' + bg.getBillablePercentWeek() * 100 + '%' +
     '</div>' +
     '<div style="color:' + (bg.getBillablePercentMonth() > bg.config.minBillablePercent ? 'green': 'red') + '">' +
       '<h2>Month</h2>' +
-      'Total: ' + round(bg.times.month.billable + bg.times.month.nonBillable) + 'h /' + (bg.config.minDayHours * 5) + 'h<br>' +
+      'Total: ' + round(bg.times.month.billable + bg.times.month.nonBillable) + 'h / ' + (bg.config.minDayHours * 5) + 'h<br>' +
       'Billable: ' + bg.getBillablePercentMonth() * 100 + '%' +
     '</div>';
 document.write(msg);
